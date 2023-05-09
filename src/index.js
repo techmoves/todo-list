fbb9368c55d9e47b7a278da8ecad6693972646b9
techmoves <muhndlovu28@gmail.com>
2023-05-10 00:27:39 +0200

    Add web pack config and todo list app

---
+++ src/index.js
@@ -0,0 +1,31 @@
+import './style.css';
+
+const mylist = [
+  {
+    description: 'Watch coding tutorails',
+    completed: false,
+    index: 1,
+  },
+  {
+    description: 'Pickup the kids at school',
+    completed: false,
+    index: 2,
+  },
+  {
+    description: 'Buy  groceries',
+    completed: false,
+    index: 3,
+  },
+
+];
+
+const list = document.getElementById('list');
+
+mylist.sort((a, b) => a.index - b.index);
+mylist.forEach((value) => {
+  const li = document.createElement('li');
+  li.innerHTML = `
+        ${value.description}<i class="fa-solid fa-ellipsis-vertical"></i>
+    `;
+  list.appendChild(li);
+});
