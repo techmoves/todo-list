e3c9e746e7dd2da4c3afd02788aee37c106d9400
techmoves <muhndlovu28@gmail.com>
2023-05-10 22:20:14 +0200

    config dist files

--- webpack.config.js
+++ webpack.config.js
@@ -2,24 +2,17 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
-  mode: 'development',
-  devServer: {
-    static: './dist',
-  },
-  plugins: [
+  entry: {
+    index: './src/index.js'
+},
+plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
     }),
   ],
   output: {
-    filename: 'main.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    clean: true,
-    publicPath: '/',
-  },
-  optimization: {
-    runtimeChunk: 'single',
   },
   module: {
     rules: [
@@ -27,11 +20,6 @@ module.exports = {
         test: /\.css$/i,
 
         use: ['style-loader', 'css-loader'],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-
-        type: 'asset/resource',
       },
     ],
   },
