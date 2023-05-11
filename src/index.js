f507d1a1e2c25063500a9e0249eb24100c6a6512
techmoves <muhndlovu28@gmail.com>
2023-05-11 23:29:38 +0200

    Add the updated list and delete  fuctionality

--- src/index.js
+++ src/index.js
@@ -1,39 +1,59 @@
 import './style.css';
-//import for add and remove
 
 import script  from '../modules/script';
 
 
-const mylist = [
-  {
-    description: 'Watch coding Tutorial on YouTube',
-    completed: false,
-    index: 1,
-  },
-  {
-    description: 'Pickup the kids at school',
-    completed: false,
-    index: 2,
-  },
-  {
-    description: 'Buy  groceries',
-    completed: false,
-    index: 3,
-  },
-
-];
-
-const list = document.getElementById('list');
-
-mylist.sort((a, b) => a.index - b.index);
-mylist.forEach((value) => {
-  const li = document.createElement('li');
-  li.innerHTML = `
-        ${value.description}<i class="fa-solid fa-ellipsis-vertical"></i>
-
-    `;
-  list.appendChild(li);
-});
 
 
 // add amd remove
+const localData = script.retrieve();
+if (!localData) localStorage.setItem("mylist", "[]");
+
+const display = () => {
+  const storeData = script.retrieve();
+
+  const list = document.getElementById("list");
+  list.innerHTML = "";
+  storeData.forEach((value) => {
+    // Creating list of to-do
+    const li = document.createElement("li");
+    li.innerHTML = `
+    <input type="checkbox">
+    <input class="text" type="text" value="${value.description}"/> 
+    <i class="fa-solid fa-ellipsis-vertical"></i>
+    `;
+    const removeButton = document.createElement("div");
+    removeButton.innerHTML = '<i class="fa-regular fa-trash-can"></i>';
+    removeButton.addEventListener("click", () => {
+      script.remove(value.id);
+      display();
+    });
+    li.appendChild(removeButton);
+    list.appendChild(li);
+  });
+
+  const span = document.querySelectorAll(".text");
+  span.forEach((btn, index) => {
+    btn.addEventListener("keyup", () => {
+      const test = script.retrieve();
+      test[index].description = btn.value;
+      script.save(test);
+    });
+  });
+};
+
+const renderList = () => {
+  const form = document.getElementById("form");
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const storeData = script.retrieve();
+    const input = form.text.value;
+    const completed = false;
+    const id = storeData.length;
+
+    script.add(input, completed, id);
+    display();
+    form.text.value = "";
+  });
+};
+renderList();
